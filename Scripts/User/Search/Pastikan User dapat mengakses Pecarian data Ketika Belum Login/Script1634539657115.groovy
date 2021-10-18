7326496a14e5ec19f655223ce1b99f12751174e3
ardiansyah <aa9717797@gmail.com>
2021-10-18 14:35:06 +0700

    test suites pencarian data

--- Scripts/User/Search/Pastikan User dapat mengakses Pecarian data Ketika Belum Login/Script1634539657115.groovy
+++ Scripts/User/Search/Pastikan User dapat mengakses Pecarian data Ketika Belum Login/Script1634539657115.groovy
@@ -17,12 +17,25 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-WebUI.callTestCase(findTestCase('User/Login/Positive Case/Pastikan User dapat melakukan Login'), [('username') : 'fitrianisa@yopmail.com'
-        , ('password') : '123Abcde'], FailureHandling.STOP_ON_FAILURE)
+WebUI.callTestCase(findTestCase('CustomKeyword/Open Browser'), [:], FailureHandling.STOP_ON_FAILURE)
+
+WebUI.callTestCase(findTestCase('CustomKeyword/Negative to URL'), [:], FailureHandling.STOP_ON_FAILURE)
+
+WebUI.click(findTestObject('Login/button__z-2 absolute right-0 top-0 bg-white w-10 h-10 flex rounded-full justify-center items-center border-black-300 border-2.5'))
+
+WebUI.focus(findTestObject('Search/Page_Pijar Mahir/button_Pijar Camp'))
 
 WebUI.click(findTestObject('Search/input_Kategori_search'))
 
-WebUI.setText(findTestObject('Search/input_Kategori_search'), 'kursus test')
+WebUI.setText(findTestObject('Search/input_Kategori_search'), 'Kopi')
+
+WebUI.click(findTestObject('Search/button_Kategori_w-10 h-full flex justify-center items-center bg-black-200'))
+
+WebUI.click(findTestObject('Search/Page_Pijar Mahir/img_Kategori_mdmr-12 mr-2 cursor-pointer'))
+
+WebUI.click(findTestObject('Search/input_Kategori_search'))
+
+WebUI.setText(findTestObject('Search/input_Kategori_search'), 'Tips Jago')
 
 WebUI.click(findTestObject('Search/button_Kategori_w-10 h-full flex justify-center items-center bg-black-200'))
 
--- Test Suites/Search Data/Pastikan user dapat melakukan pencarian data.ts
+++ Test Suites/Search Data/Pastikan user dapat melakukan pencarian data.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>Pastikan user dapat melakukan pencarian data ketika login</name>
+   <name>Pastikan user dapat melakukan pencarian data</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
@@ -12,9 +12,15 @@
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>f0194cf1-afe3-4afa-8fde-8cfa5c9a08a7</testSuiteGuid>
    <testCaseLink>
+      <guid>e6139d09-fe47-471b-b0eb-746e09ac8988</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/User/Search/Pastikan User dapat mengakses Pecarian data Ketika Belum Login</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
       <guid>4a9df0eb-7fb4-4f57-a8a3-0c13e87541a9</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/User/Search/Pastikan User dapat mengakses Pecarian data</testCaseId>
+      <testCaseId>Test Cases/User/Search/Pastikan User dapat mengakses Pecarian data Ketika Login</testCaseId>
    </testCaseLink>
 </TestSuiteEntity>
